e323725d1f68fff234974e2802dd12886bebc95b
LasseMempel <lassemempel@gmail.com>
2024-07-29 15:05:59 +0200

    ontology visualization update

--- js/visualizeOntology.js
+++ js/visualizeOntology.js
@@ -4,6 +4,7 @@
         for (let object of jsonld) {
             let node = {};
             if (object["@type"] == "http://www.w3.org/2002/07/owl#Class") {
+                node = {};
                 node.id = stripOwlPrefix(object["@id"]);
                 node.group = 1;
                 if (object.hasOwnProperty("http://www.w3.org/2000/01/rdf-schema#subClassOf")) {
@@ -16,16 +17,47 @@
                         data.links.push(link);
                     }
                 }
-                data.nodes.push(node)
+                data.nodes.push(node);
             }
+            
             if (object["@type"] == "http://www.w3.org/2002/07/owl#ObjectProperty") {
+                /*
+                node = {};
+                node.id = stripOwlPrefix(object["@id"]);
+                node.group = 2;
+                let domainId
+                let rangeId
+                let link = {};
+                for (let domain of object["http://www.w3.org/2000/01/rdf-schema#domain"]) {
+                    link = {}
+                    domainId = stripOwlPrefix(domain["@id"]);
+                    link.source = domainId;
+                    link.target = node.id;
+                    link.value = 1;
+                    //link.name = node.id;
+                    data.links.push(link);
+                }
+                for (let range of object["http://www.w3.org/2000/01/rdf-schema#range"]) {
+                    link = {}
+                    rangeId = stripOwlPrefix(range["@id"]);
+                    link.source = node.id;
+                    link.target = rangeId;
+                    link.value = 1;
+                    //link.name = node.id;
+                    data.links.push(link);
+                }
+                data.nodes.push(node);
+            }
+            */
+                node = {};
                 node.id = stripOwlPrefix(object["@id"]);
                 let domainId
                 let rangeId
+                let link;
                 for (let domain of object["http://www.w3.org/2000/01/rdf-schema#domain"]) {
                     domainId = stripOwlPrefix(domain["@id"]);
                     for (let range of object["http://www.w3.org/2000/01/rdf-schema#range"]) {
-                        let link = {};
+                        link = {}
                         rangeId = stripOwlPrefix(range["@id"]);
                         link.source = domainId;
                         link.target = rangeId;
@@ -40,8 +72,9 @@
         let input = document.getElementById('input');
         let result = document.getElementById('result');
         result.innerHTML = '';
-        result.appendChild(generateGraph(data));
-        let output = document.getElementById("output");
+        result.appendChild(generateGraphWithLabels(data));
+        //let output = document.getElementById("output");
+        //output.innerHTML=JSON.stringify(data);
 
         /*
         for (let node of data.nodes) {
